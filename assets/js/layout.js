d99a79a5daf7b93981327ced4c631b91a1688709
Vishal Thakkar <vishal.thakkar@3insys.com>
2023-01-19 20:45:12 +0530

    update

---
+++ assets/js/layout.js
@@ -0,0 +1,8 @@
+﻿const header = `This is Header`;
+const footer = `This is Footer`;
+const title = `This is Title`;
+(function () {
+    document.getElementById('title').innerHTML = title;
+    document.getElementById('header').innerHTML = header;
+    document.getElementById('footer-area').innerHTML = footer;
+})();
