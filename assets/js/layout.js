aa262bd45c4c874ad45216042ee411c4e6cba4e7
Vishal Thakkar <vishal.thakkar@3insys.com>
2023-01-23 23:30:29 +0530

    update

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,6 +1,9 @@
 ﻿const header = `This is Header`;
 const footer = `This is Footer`;
 const title = `This is Title`;
+function readMoreLess() {
+    this.getAttribute('aria-expanded') == 'true' ? this.textContent = 'Read Less' : this.textContent = 'Read More';
+}
 (function () {
     document.getElementById('title').innerHTML = title;
     document.getElementById('header').innerHTML = header;
